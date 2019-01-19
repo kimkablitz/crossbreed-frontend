ce6fb188092307577f73a25e91e546d219d44b57
Kona Tess <konatess@gmail.com>
2019-01-18 20:02:30 -0800

    Accepted changes.

--- App.js
+++ App.js
@@ -27,30 +27,11 @@ export default class App extends React.Component {
     );
   }
 
-<<<<<<< HEAD
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-        require('./assets/images/testPet.svg'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
-  };
-=======
   async _cacheResourcesAsync() {
     const images = [
       require('./assets/images/robot-dev.png'),
       require('./assets/images/robot-prod.png'),
     ];
->>>>>>> 1b0d3e37e1d466ff623fe5510fb6b600497cce90
 
     const cacheImages = images.map((image) => {
       return Asset.fromModule(image).downloadAsync();
