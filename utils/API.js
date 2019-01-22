a2931d7b1a6a35e751327fa67bc4d00f1c171cbc
J.Lee <joverlee521@gmail.com>
2019-01-21 19:47:01 -0800

    added login and sign up page for android, currently login is hooked up to arron's reactnativebooksapp's apiwill be changed to our own api once that has been deployed

--- utils/API.js
+++ utils/API.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/"
+const baseURL = "http://localhost:3001/"
 
 export default {
     testRoute: () => {
         return axios.get(baseURL);
+    },
+    login: (userObj) => {
+        return axios.post("https://uw-playground-api.herokuapp.com/auth/login", userObj);
     }
 }
