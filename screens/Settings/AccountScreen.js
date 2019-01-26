c223e1da62674b9a6c7ef9f05b9f27faf0e52bac
kimkablitz <kim.tran549@gmail.com>
2019-01-25 17:53:40 -0800

    logout button redirect to auth route

--- screens/Settings/AccountScreen.js
+++ screens/Settings/AccountScreen.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, View, FlatList } from 'react-native';
 import { Container, Header, Body, Title, Content, Button, Text } from "native-base";
-
 import API from "../../utils/API";
 import style from './style';
 import { navigationType } from '../../propTypes';
@@ -15,21 +14,32 @@ export default class AccountScreen extends React.Component {
     title: 'Account',
   };
 
-  signOut = async () => {
-    try {
-      await GoogleSignin.revokeAccess();
-      await GoogleSignin.signOut();
-      this.setState({ user: null }); // Remember to remove the user from your app's state as well
-    } catch (error) {
-      console.error(error);
+  // signOut = async () => {
+  //   try {
+  //     await GoogleSignin.revokeAccess();
+  //     await GoogleSignin.signOut();
+  //     this.setState({ user: null }); // Remember to remove the user from your app's state as well
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  logout = () => {
+    API.logout()
+    .then( () =>{
+      const navigateHome = NavigationActions.navigate({
+        routeName: "Auth",
+      });
+      this.props.navigation.dispatch(navigateHome);
     }
-  };
+    )
+    .catch(err => this.setState({signedIn: false}))
+  }
 
   render() {
     console.log("hey")
     return (
       <View style={style.listContainer}>
-        <Button block danger onPress={() => { this.signOut.onPress(); }}>
+        <Button block danger onPress={() => this.logout()} >
           <Text>Log Out</Text>
         </Button>
       </View>
