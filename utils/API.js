3256991db20c7d83900fabb0e6abcf4610a17093
J.Lee <joverlee521@gmail.com>
2019-01-26 11:04:12 -0800

    changed keys for all .maps to match pet._id

--- utils/API.js
+++ utils/API.js
@@ -14,5 +14,11 @@ export default {
     },
     googleLogin: (googleUserObj) => {
         return axios.post(`${baseURL}/auth/login/google`, googleUserObj);
+    },
+    updateUsername: (userId, newUsername) => {
+        return axios.put(`${baseURL}/api/users/${userId}`, newUsername);
+    },
+    updateLevelAndXP: (petId, levelObj) => {
+        return axios.put(`${baseURL}/api/pets/${petId}`, levelObj);
     }
 }
