5e4a220d64be7525fea40725dcde61762df02b64
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-01-28 11:36:22 -0800

    Fixing font load async issue

--- App.js
+++ App.js
@@ -112,6 +112,9 @@ export default class App extends React.Component {
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+        Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
       }),
     ]);
   };
