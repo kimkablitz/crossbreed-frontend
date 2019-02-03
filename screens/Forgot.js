0f3ae6ce979dba24930253768d982cfd30e8a7b4
kimkablitz <kim.tran549@gmail.com>
2019-02-02 20:41:47 -0800

    added message trying install flash in forgot.js

--- screens/Forgot.js
+++ screens/Forgot.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, AsyncStorage } from "react-native"; 
-import { Container, Header, Content, Form, Item, Input, Label, Button, Text, Icon } from 'native-base';
+import { View, AsyncStorage, Button } from "react-native";
+import { Container, Header, Content, Form, Item, Input, Label, Text, Icon } from 'native-base';
+import { showMessage, hideMessage } from "react-native-flash-message";
+
 
 export default class Forgot extends Component {
 
@@ -8,26 +10,35 @@ export default class Forgot extends Component {
         email: ''
     }
 
-    reset = () =>{
+    reset = () => {
         console.log("love you")
     }
-  render() {
-    return (
-        <Container>
-        <Content padder contentContainerStyle={{ flex: 1 }}>
-          <Form>
-            <Item floatingLabel>
-              <Label>Email</Label>
-              <Input name="displayName" onChangeText={(value) => this.setState({email: value.trim()})}/>
-            </Item>
-            <View style={{ marginVertical: 20 }}>
-               <Button block onPress={this.reset()}> 
-                <Text>Reset Password</Text>
-               </Button>
-            </View>
-          </Form> 
-        </Content>
-      </Container>
-    )
-  }
+
+    render() {
+        return (
+            <Container>
+                <Content padder contentContainerStyle={{ flex: 1 }}>
+                    <Form>
+                        <Item floatingLabel>
+                            <Label>Email</Label>
+                            <Input name="email" onChangeText={(value) => this.setState({ email: value.trim() })} />
+                        </Item>
+                        <View style={{ marginVertical: 20 }}>
+                            <Button
+                                onPress={() => {
+                                    /* HERE WE GONE SHOW OUR FIRST MESSAGE */
+                                    showMessage({
+                                        message: "Simple message",
+                                        type: "info",
+                                    });
+                                }}
+                                title="Reset Password"
+                                color="#841584"
+                            />
+                        </View>
+                    </Form>
+                </Content>
+            </Container>
+        )
+    }
 }
